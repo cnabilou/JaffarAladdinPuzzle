16a02f33992b772be92d43db66162ef5adfc1a50
Nabile Coulibaly <cnabilou@gmail.com>
2018-02-25 03:18:13 -0600

    Struggling with multi row options

--- Tests/SolutionTests.cs
+++ Tests/SolutionTests.cs
@@ -54,12 +54,12 @@ namespace Tests
         }
 
         [TestMethod]
-        public void Should_Return_Five_Jump_As_MaxJump()
+        public void Should_Return_Six_Jump_As_MaxJump()
         {
             var actual = new[]
             {
                 ".............",
-                ".............",
+                ".....X.......",
                 ".............",
                 ".....X.......",
                 ".............",
@@ -73,7 +73,7 @@ namespace Tests
                 "......O......"
             };
             var expected = Solution.GetMaxJump(actual);
-            Assert.AreEqual(expected, 5, "The max jump should be 6");
+            Assert.AreEqual(expected, 6, "The max jump should be 6");
         }
 
         #region Should be private Methods
@@ -94,30 +94,6 @@ namespace Tests
             Assert.IsNotNull(expected);
             Assert.AreEqual(expected[0], 3, "X should be equal to 3");
             Assert.AreEqual(expected[1], 5, "The Y should be equal to 5");
-        }
-
-        [TestMethod]
-        public void Jump()
-        {
-            var actual = new[]
-            {
-                "............",
-                "............",
-                "........X...",
-                "............",
-                "........X.X.",
-                ".........O..",
-                "............",
-                "............",
-                "............",
-                "............",
-                "............",
-                "............"
-            };
-
-            var expected = Solution.Jump(new int[] { 9, 5 }, actual);
-            Console.WriteLine("Max jump: " + expected);
-            Assert.AreEqual(expected, 2, "The max jump should be 2");
         }
 
         [TestMethod]
--- jaffar_aladdin_puzzle/Solution.cs
+++ jaffar_aladdin_puzzle/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Jafar_Aladdin
 {
@@ -13,6 +14,8 @@ namespace Jafar_Aladdin
         private static int _leftMoveCount = 0;
         private static int _rightMoveCount = 0;
 
+        private static int _zzCounter = 0;
+
         private static int[] _aladdinInitialPosition = new int[] { };
 
         private static int _arrayDimension = 0;
@@ -21,19 +24,26 @@ namespace Jafar_Aladdin
         private static int _ly = 0;
         private static int[] _newPosition = new int[] { };
 
+        private static List<int> _pathMaxJump = new List<int> { };
+
         #endregion Private Members
 
         public static int GetMaxJump(string[] array)
         {
             // Get array dimension
             _arrayDimension = array.Length - 1;
+
+            Console.WriteLine($"Array height: {_arrayDimension}");
+            Console.WriteLine($"Array width: {array[0].Length - 1}");
+
             // Get Aladdin position
             _aladdinInitialPosition = AladdinPostion(array);
 
-            LeftPathJumpCount(_aladdinInitialPosition, array);
-            RightPathJumpCount(_aladdinInitialPosition, array);
-
-            return Math.Max(_leftMoveCount, _rightMoveCount);
+            var lc = LeftPathJumpCount(_aladdinInitialPosition, array);
+            var rc = RightPathJumpCount(_aladdinInitialPosition, array);
+            var zz = ZigZagPathJumpCount(_aladdinInitialPosition, array);
+            var max1 = Math.Max(lc, rc);
+            return Math.Max(max1, zz);
         }
 
         /// <summary>
@@ -54,49 +64,6 @@ namespace Jafar_Aladdin
                 arrayRow++;
             }
             return null;
-        }
-
-        /// <summary>
-        /// Calculate the maximum number of jumps on the list of string
-        /// </summary>
-        /// <param name="aladdinPosition"></param>
-        /// <param name="array"></param>
-        /// <returns></returns>
-        public static int Jump(int[] aladdinPosition, string[] array)
-        {
-            if (CanMoveToPosition(aladdinPosition, array, SearchType.CanMoveLeft))
-            {
-                _leftMoveCount++;
-
-                // get new aladdin position
-                _lx = aladdinPosition[0] - 2;
-                _ly = aladdinPosition[1] - 2;
-
-                if ((_lx >= 0 || _lx <= _arrayDimension)
-                    || (_ly >= 0 || _ly <= _arrayDimension))
-                {
-                    _newPosition = new int[] { _lx, _ly };
-                    Jump(_newPosition, array);
-                }
-            }
-            if (CanMoveToPosition(aladdinPosition, array, SearchType.CanMoveRight))
-            {
-                _rightMoveCount++;
-
-
-                // get new aladdin position
-                _lx = aladdinPosition[0] + 2;
-                _ly = aladdinPosition[1] - 2;
-
-                if ((_lx >= 0 || _lx <= _arrayDimension)
-                    || (_ly >= 0 || _ly <= _arrayDimension))
-                {
-                    _newPosition = new int[] { _lx, _ly };
-                    Jump(_newPosition, array);
-                }
-            }
-
-            return Math.Max(_leftMoveCount, _rightMoveCount);
         }
 
         public static bool CanMoveToPosition(int[] position, string[] array, SearchType searchType)
@@ -131,8 +98,9 @@ namespace Jafar_Aladdin
 
                 case SearchType.CanMoveRight:
                     // Need to check that we are not going to go out of the bound of the array
-                    if ((y - 1 < 0) || (x + 1 > _arrayDimension)
-                        || (y - 2 < 0) || (x + 2 > _arrayDimension))
+                    if ((x + 2 > _arrayDimension)
+                        || (y - 2 < 0)
+                        || (x + 2 > _arrayDimension))
                     {
                         return false;
                     }
@@ -151,76 +119,136 @@ namespace Jafar_Aladdin
             }
         }
 
-        public static void LeftPathJumpCount(int[] aladdinPosition, string[] array)
+        public static int LeftPathJumpCount(int[] aladdinPosition, string[] array)
         {
-            var lx = 0;
-            var ly = 0;
+            Console.WriteLine($"Starting Aladdin position Path1: {aladdinPosition[0]}, {aladdinPosition[1]}");
 
             if (CanMoveToPosition(aladdinPosition, array, SearchType.CanMoveLeft))
             {
                 _leftMoveCount++;
 
+                var lx = 0;
+                var ly = 0;
+
                 // get new aladdin position
                 lx = aladdinPosition[0] - 2;
                 ly = aladdinPosition[1] - 2;
 
-                if ((lx >= 0)
-                    || (ly >= 0))
+                if ((lx > 0)
+                    && (ly > 0))
                 {
                     var newPosition = new int[] { lx, ly };
                     LeftPathJumpCount(newPosition, array);
                 }
             }
-            if (CanMoveToPosition(aladdinPosition, array, SearchType.CanMoveRight))
+            else if (CanMoveToPosition(aladdinPosition, array, SearchType.CanMoveRight))
             {
-                _rightMoveCount++;
+                _leftMoveCount++;
+
+                var lx = 0;
+                var ly = 0;
+
                 // get new aladdin position
                 lx = aladdinPosition[0] + 2;
                 ly = aladdinPosition[1] - 2;
 
-                if ((lx >= 0 || _lx <= _arrayDimension)
-                    || (_ly >= 0))
+                if ((ly > 0)
+                    && (lx < _arrayDimension))
                 {
                     var newPosition = new int[] { lx, ly };
                     LeftPathJumpCount(newPosition, array);
                 }
             }
+
+            return _leftMoveCount;
         }
 
-        public static void RightPathJumpCount(int[] aladdinPosition, string[] array)
+        public static int RightPathJumpCount(int[] aladdinPosition, string[] array)
         {
-            var lx = 0;
-            var ly = 0;
+            Console.WriteLine();
+            Console.WriteLine($"Starting Aladdin position Path2: {aladdinPosition[0]}, {aladdinPosition[1]}");
 
             if (CanMoveToPosition(aladdinPosition, array, SearchType.CanMoveRight))
+            {
+                var lx = 0;
+                var ly = 0;
+
+                _rightMoveCount++;
+
+                // get new aladdin position
+                lx = aladdinPosition[0] + 2;
+                ly = aladdinPosition[1] + 2;
+
+
+                if ((lx < _arrayDimension)
+                    && (ly < _arrayDimension))
+                {
+                    var newPosition = new int[] { lx, ly };
+                    RightPathJumpCount(newPosition, array);
+                }
+            }
+            else if (CanMoveToPosition(aladdinPosition, array, SearchType.CanMoveLeft))
             {
                 _rightMoveCount++;
 
                 // get new aladdin position
-                lx = aladdinPosition[0] - 2;
-                ly = aladdinPosition[1] - 2;
-
-                if ((lx >= 0)
-                    || (ly >= 0))
-                {
-                    var newPosition = new int[] { lx, ly };
-                    RightPathJumpCount(newPosition, array);
-                }
-            }
-            if (CanMoveToPosition(aladdinPosition, array, SearchType.CanMoveLeft))
-            {
-                _leftMoveCount++;
-                // get new aladdin position
+                var lx = 0;
+                var ly = 0;
                 lx = aladdinPosition[0] + 2;
                 ly = aladdinPosition[1] - 2;
 
-                if ((lx >= 0 || _lx <= _arrayDimension)
-                    || (_ly >= 0))
+                if ((lx < _arrayDimension)
+                    && (ly > 0))
                 {
+
                     var newPosition = new int[] { lx, ly };
                     RightPathJumpCount(newPosition, array);
                 }
             }
+            return _rightMoveCount;
+        }
+
+        public static int ZigZagPathJumpCount(int[] aladdinPosition, string[] array)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Starting Aladdin position Path3: {aladdinPosition[0]}, {aladdinPosition[1]}");
+
+            if (CanMoveToPosition(aladdinPosition, array, SearchType.CanMoveLeft))
+            {
+                _zzCounter++;
+
+                // get new aladdin position
+                var lx = 0;
+                var ly = 0;
+                lx = aladdinPosition[0] + 2;
+                ly = aladdinPosition[1] + 2;
+
+                if ((lx < _arrayDimension)
+                    && (ly < _arrayDimension))
+                {
+                    var newPosition = new int[] { lx, ly };
+                    LeftPathJumpCount(newPosition, array);
+                }
+            }
+            if (CanMoveToPosition(aladdinPosition, array, SearchType.CanMoveRight))
+            {
+                _zzCounter++;
+                // get new aladdin position
+                var lx = 0;
+                var ly = 0;
+                lx = aladdinPosition[0] + 2;
+                ly = aladdinPosition[1] - 2;
+
+                if ((lx < _arrayDimension)
+                    && (ly > 0))
+                {
+
+                    var newPosition = new int[] { lx, ly };
+                    RightPathJumpCount(newPosition, array);
+                }
+            }
+            Console.WriteLine($"");
+            return _zzCounter;
         }
     }
 }
